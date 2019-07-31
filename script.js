93bf7c1378bde7dedf571882ce49a829aea6605c
Pulkit Aggarwal <31651398+BoeingHawk@users.noreply.github.com>
2019-08-01 03:40:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+document.getElementById('buttonid').addEventListener('click', openDialog);
+
+function openDialog() {
+  document.getElementById('fileid').click();
+}
+console.log(fileid)
